ca3a3ff01ab0fb676f0c532d54f3a94a5ed1f188
lxaw <LAWHALEN@email.sc.edu>
2023-01-26 14:48:40 -0500

    styling menustat form, navbar

--- src/Form/MenustatFoodFormType.php
+++ src/Form/MenustatFoodFormType.php
@@ -4,6 +4,8 @@ namespace App\Form;
 
 use App\Entity\MenustatFood;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -46,6 +48,12 @@ class MenustatFoodFormType extends AbstractType
             ]
             )
             ->add('ServingSize'
+                ,IntegerType::class,[
+                    'attr'=> array(
+                        'class'=>'form-control',
+                        'type'=>'number'
+                    )
+                ]
             )
             ->add('ServingSizeText'
             ,TextType::class,[
@@ -71,19 +79,97 @@ class MenustatFoodFormType extends AbstractType
                 )
             ]
             )
-            ->add('EnergyAmount')
-            ->add('FatAmount')
-            ->add('SaturatedFatAmount')
-            ->add('TransFatAmount')
-            ->add('CholesterolAmount')
-            ->add('SodiumAmount')
-            ->add('PotassiumAmount')
-            ->add('CarbAmount')
-            ->add('ProteinAmount')
-            ->add('SugarAmount')
-            ->add('FiberAmount')
-            ->add('Date')
-            ->add('Quantity')
+            ->add('EnergyAmount'
+            ,IntegerType::class,[
+                'attr'=>array(
+                    'class'=>'form-control',
+                    'type'=>'number'
+                )
+            ]
+            )
+            ->add('FatAmount'
+            ,IntegerType::class,[
+                'attr'=>array(
+                    'class'=>'form-control',
+                    'type'=>'number'
+                )
+            ])
+            ->add('SaturatedFatAmount'
+            ,IntegerType::class,[
+                'attr'=>array(
+                    'class'=>'form-control',
+                    'type'=>'number'
+                )
+            ])
+            ->add('TransFatAmount'
+            ,IntegerType::class,[
+                'attr'=>array(
+                    'class'=>'form-control',
+                    'type'=>'number'
+                )
+            ])
+            ->add('CholesterolAmount'
+            ,IntegerType::class,[
+                'attr'=>array(
+                    'class'=>'form-control',
+                    'type'=>'number'
+                )
+            ])
+            ->add('SodiumAmount'
+            ,IntegerType::class,[
+                'attr'=>array(
+                    'class'=>'form-control',
+                    'type'=>'number'
+                )
+            ])
+            ->add('PotassiumAmount'
+            ,IntegerType::class,[
+                'attr'=>array(
+                    'class'=>'form-control',
+                    'type'=>'number'
+                )
+            ])
+            ->add('CarbAmount'
+            ,IntegerType::class,[
+                'attr'=>array(
+                    'class'=>'form-control',
+                    'type'=>'number'
+                )
+            ])
+            ->add('ProteinAmount'
+            ,IntegerType::class,[
+                'attr'=>array(
+                    'class'=>'form-control',
+                    'type'=>'number'
+                )
+            ])
+            ->add('SugarAmount'
+            ,IntegerType::class,[
+                'attr'=>array(
+                    'class'=>'form-control',
+                    'type'=>'number'
+                )
+            ])
+            ->add('FiberAmount'
+            ,IntegerType::class,[
+                'attr'=>array(
+                    'class'=>'form-control',
+                    'type'=>'number'
+                )
+            ])
+            ->add('Date'
+            ,DateType::class,[
+                'attr'=>array(
+                    'class'=>'form-control',
+                )
+            ])
+            ->add('Quantity'
+            ,IntegerType::class,[
+                'attr'=>array(
+                    'class'=>'form-control',
+                    'type'=>'number'
+                )
+            ])
         ;
     }
 
