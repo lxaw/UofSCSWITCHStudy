829a3541e9e145cd20f6d5355625028bdffbcbd7
lxaw <LAWHALEN@email.sc.edu>
2023-01-27 10:48:06 -0500

    calendar grabs the dates

--- assets/react/controllers/Calendar.jsx
+++ assets/react/controllers/Calendar.jsx
@@ -2,31 +2,46 @@
 import React, { useState } from 'react';
 
 import Calendar from 'react-calendar';
+import 'react-calendar/dist/Calendar.css'
 import './calendar.css'
 
 import moment from 'moment/moment';
 
 export default function (props) {
   const [date, setDate] = useState(new Date());
-  
-  const style = {color:'red'}
 
+  const clickDate = (dateObj) =>{
+    const strFormattedDate = moment(dateObj).format('YYYY-MM-DD');
+    // document.location.href = `/food/date/${strFormattedDate}/`
+    if(props.marks.includes(strFormattedDate)){
+      
+    }else{
+      console.log('there')
+    }
+    setDate(dateObj);
+  }
+  
   return (
-    <div className='app'>
+    <div className='app justify-content-center'>
       <div className='calendar-container'>
         <Calendar 
-            onChange={setDate} 
+            onChange={clickDate} 
             value={date} 
             locale="en-EN"
+            tileContent = {
+              ({activeStateDate, date, view}) => {
+                return view === "month" && date.getDay() === 0 
+                ? <span onClick={clickDate}></span> : null
+              }
+            }
             tileClassName={({ date, view }) => {
               if(props.marks.find(x=>x===moment(date).format("YYYY-MM-DD"))){
-                console.log(date)
-                return style
+                return "highlight"
               }
             }}
         />
       </div>
-      <p className='text-center'>
+      <p>
         <span className='bold'>Selected Date:</span>{' '}
         {date.toDateString()}
       </p>
--- src/Controller/FoodController.php
+++ src/Controller/FoodController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Doctrine\ORM\EntityManagerInterface;
 
-// food entities and repositories
+// food entities and repositorie containers
 use App\Entity\MenustatFood;
 use App\Repository\MenustatRepository;
 use App\Entity\UsdaBrandedFood;
